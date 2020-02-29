f48d475c48a8b1f6d0a48d16f4ec6ef8eed42423
tguo9 <tguo9@dons.usfca.edu>
2020-02-28 17:17:41 -0800

    add draft docsting for stats summary

---
+++ R/stat_summary.R
@@ -0,0 +1,14 @@
+#' Create a stats summary
+#'
+#' This function creates a population from a distribution.
+#' Author: Tao Guo
+#'
+#'@param population, samples, parameter
+#'
+#'@return a tibble summary stats
+#'
+#'@examples
+#'stat_summary <- function(pop, samples, parameter)
+stat_summary <- function(population, samples, parameter) {
+
+}

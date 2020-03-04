7c8025d5d7d70be11866e49f19aa3d3d10891086
tguo9 <tguo9@dons.usfca.edu>
2020-03-03 21:05:46 -0800

    add tests for generate_virtual_pop. Close #29

--- tests/testthat/test-generate_virtual_pop.R
+++ tests/testthat/test-generate_virtual_pop.R
@@ -1,4 +1,5 @@
 test_that("multiplication works", {
   expect_equal(colnames(generate_virtual_pop(100, "test_name", rnorm, 0, 1)), 'test_name')
-
+  expect_equal(nrow(generate_virtual_pop(100, 'var_name', rnorm, 0, 1)), 100)
+  expect_equal(ncol(generate_virtual_pop(100, 'var_name', rnorm, 0, 1)), 1)
 })

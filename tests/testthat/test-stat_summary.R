37cad3f3d25705de4a27e6bcea09322823543c6c
tguo9 <tguo9@dons.usfca.edu>
2020-03-04 10:09:20 -0800

    add two more tests for stat_summary

--- tests/testthat/test-stat_summary.R
+++ tests/testthat/test-stat_summary.R
@@ -1,3 +1,5 @@
 test_that("multiplication works", {
+  expect_equal(stat_summary(dplyr::tibble(x = 1:3), dplyr::tibble(x = 1:3), 'mean')[['mean']], c(2, 2))
+  expect_equal(nrow(stat_summary(dplyr::tibble(x = 1:10), dplyr::tibble(x = 1:10), c('mean', 'sd'))), 2)
   expect_equal(names(stat_summary(dplyr::tibble(x = 1:10), dplyr::tibble(x = 1:10), c('mean', 'sd'))), c('data', 'mean', 'sd'))
 })

26c6de9faf1d0d61213ff4a67b544e1e2e19e62e
tguo9 <tguo9@dons.usfca.edu>
2020-03-03 22:57:29 -0800

    Update passing message

--- tests/testthat/test-generate_virtual_pop.R
+++ tests/testthat/test-generate_virtual_pop.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("Create a virtual population works", {
   expect_equal(colnames(generate_virtual_pop(100, "test_name", rnorm, 0, 1)), 'test_name')
   expect_equal(nrow(generate_virtual_pop(100, 'var_name', rnorm, 0, 1)), 100)
   expect_equal(ncol(generate_virtual_pop(100, 'var_name', rnorm, 0, 1)), 1)

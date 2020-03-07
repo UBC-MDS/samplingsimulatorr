50b8a60406812f31e095cba2f8911be8a2f8438b
Tao <tguo9@dons.usfca.edu>
2020-03-07 11:41:41 -0800

    Update tests/testthat/test-stat_summary.R
    Fix wording
    
    Co-Authored-By: hwilliams10 <54678828+hwilliams10@users.noreply.github.com>

--- tests/testthat/test-stat_summary.R
+++ tests/testthat/test-stat_summary.R
@@ -1,4 +1,4 @@
-test_that("The tibble not generate as expected", {
+test_that("The tibble does not generate as expected", {
   expect_equal(stat_summary(dplyr::tibble(x = 1:3), dplyr::tibble(x = 1:3), 'mean')[['mean']], c(2, 2))
   expect_equal(nrow(stat_summary(dplyr::tibble(x = 1:10), dplyr::tibble(x = 1:10), c('mean', 'sd'))), 2)
   expect_equal(names(stat_summary(dplyr::tibble(x = 1:10), dplyr::tibble(x = 1:10), c('mean', 'sd'))), c('data', 'mean', 'sd'))

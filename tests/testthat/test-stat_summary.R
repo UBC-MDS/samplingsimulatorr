4f37f7b38e215077c404d4840f889ad385e36327
Tao <tguo9@dons.usfca.edu>
2020-03-07 11:40:50 -0800

    Update tests/testthat/test-stat_summary.R
    Per Holly's suggestion
    
    Co-Authored-By: hwilliams10 <54678828+hwilliams10@users.noreply.github.com>

--- tests/testthat/test-stat_summary.R
+++ tests/testthat/test-stat_summary.R
@@ -10,7 +10,7 @@ test_that("The population you passed in is not a valid data frame", {
   expect_error(stat_summary(dplyr::tibble(a = numeric()), dplyr::tibble(x = 1:10), c('mean', 'sd')))
 })
 
-test_that("The psamples you passed in is not a valid data frame", {
+test_that("The samples you passed in is not a valid data frame", {
   expect_error(stat_summary(dplyr::tibble(x = 1:3), c(1, 2), 'mean'))
   expect_error(stat_summary(dplyr::tibble(x = 1:10), dplyr::tibble(a = character()), c('mean', 'sd')))
   expect_error(stat_summary(dplyr::tibble(x = 1:10),  dplyr::tibble(a = numeric()), c('mean', 'sd')))

d7baccb8f1e1cb15353b588a8d3c4830cbe80581
tguo9 <tguo9@dons.usfca.edu>
2020-03-25 13:16:33 -0700

    remove author line from docsting per #81

--- R/stat_summary.R
+++ R/stat_summary.R
@@ -1,7 +1,6 @@
 #' Create a stats summary
 #'
 #' This function creates a summary stats for population, samples and parameter(s) of interest.
-#' Author: Tao Guo
 #'
 #'@param population The virtual population
 #'@param samples The drawed samples
